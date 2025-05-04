e08d58837ac2636f338298965feb836b33e329af
AndreasChristianson <33329956+AndreasChristianson@users.noreply.github.com>
2025-05-04 11:04:52 -0500

    react 17 (#61)
    * react 17
    
    ncu -u
    
    * eof
    
    * regenerate lockfile
    
    * update actions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import 'fontsource-open-sans/700.css'
 import 'fontsource-open-sans/400.css'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client';
 import React from 'react'
 
-import { Homepage } from './pages/Homepage'
+import {Homepage} from './pages/Homepage'
 
-ReactDOM.render(<Homepage />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+
+root.render(<Homepage/>);
